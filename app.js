479e8781d59eb65d061e08c57469aef0c555b9d4
vancerweston <vweston@student.neumont.edu>
2020-05-14 13:46:57 -0600

    created database connection

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+// npm module imports
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let mongoose = require('mongoose');
 
+// application module imports
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// database setup
+console.log('<<-- Initiating Mongoose Test -->>');
+
+let connectiong_string = 'mongodb://127.0.0.1:27017/my_personal_library?retryWrites=true&w=majority';
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useNewUrlParser', true);
+
+mongoose.connect(connectiong_string)
+    .then(() => {
+        console.log('<<-- The MongoDB connection was successful -->>');
+
+    })
+    .catch((err) => {
+        console.log('Connection Failure: An error has occurred - error ', err);
+    });
+
 
 var app = express();
 
